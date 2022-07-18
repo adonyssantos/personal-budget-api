67cf85bacdc9fd0fda4281eb0d94fcd1b8ff5863
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-18 11:35:03 -0400

    Refactor routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,30 @@
+from app.adapters import categories_adapter, income_adapter, expenses_adapter
+from app.api import get_categories, get_first_sheet_data
+from app.utils import get_categories_balance_with_total
+from config import config
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-from app import routes
+
+@app.route("/")
+def index():
+    categories = categories_adapter(get_categories())
+    income = income_adapter(get_first_sheet_data(
+        config['api_file_name'], config['document_name']))
+    expenses = expenses_adapter(get_first_sheet_data(
+        config['api_file_name'], config['document_name']))
+
+    if sum([category['absoluteAmount'] for category in categories]) != 100:
+        raise Exception('The sum of absoluteAmounts must be 100')
+
+    categories_balance = get_categories_balance_with_total(
+        categories, income, expenses)
+
+    return render_template('index.html', categories_balance=categories_balance)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
