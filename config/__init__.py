0d41082b53e55703b7619f96b669360064a5947b
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-18 11:32:34 -0400

    Add flask API

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,6 @@
 config = {
     'api_file_name': "google-creds.json",
-    'document_name': "Budget 50-30-20 (Responses)"
+    'document_name': "Budget 50-30-20 (Responses)",
+    'port': 5000,
+    'debug': True
 }
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from src.main import main
+from app import app
+from config import config
 
+PORT = config['port']
+DEBUG = config['debug']
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(port=PORT, debug=DEBUG)
